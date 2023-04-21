99065f682378951fad62c1337690cdee4987582b
현수 조 <jhsroid@naver.com>
2023-04-21 19:49:28 +0900

    feat : 스택 어뎁터 구현과 주석으로 설명

---
+++ 230421/AdapterStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class AdapterStack<T>
+    {
+        private List<T> container;          // 리스트 기반 변수 선언
+
+        public AdapterStack()               // 생성자
+        {
+            container = new List<T>();
+        }
+
+        public void Push(T value)           // Stack 항 (최전방으로)추가
+        {
+            container.Add(value);           // 리스트의 Add함수 이용
+        }
+
+        public T Pop()                      // Stack 항(최전방) 반환 후 제거
+        {
+            T value = container[container.Count - 1];   // value에 현재 최전방 항 전해주고
+            container.RemoveAt(container.Count - 1);    // 최전방 항 제거
+            return value;                               // value 반환
+        }
+
+        public T Peek()                     // Stack 최전방 항 반환
+        {
+            return container[container.Count - 1];
+        }
+    }
+}
