61b455b431d8e676c8dbaffb924612c9cea3f3bf
현수 조 <jhsroid@naver.com>
2023-04-23 19:52:59 +0900

    feat : Queue 추가와 Player 클래스의 속도 변수와 함수 삭제

--- 230421/FastToStart.cs
+++ 230421/FastToStart.cs
@@ -8,23 +8,16 @@ namespace _230421
 {
     internal class FastToStart
     {
+        private Queue<string> players = new Queue<string>();
         public FastToStart() { }
     }
 
     internal class Player
     {
         private string name;
-        private int speed;
         public Player(string name)
         {
             this.name = name;
-            AddPlayer();
-        }
-
-        public void AddPlayer()
-        {
-            Random setSpeed = new Random();
-            speed = setSpeed.Next(1, 10);
         }
     }
 }
