9488d67aae0c32e5e07e5ada7807f516ae1bf0a3
현수 조 <jhsroid@naver.com>
2023-04-23 22:53:45 +0900

    fix : 참가인원수가 부족할 경우의 수 추가

--- 230421/Josephus permutation.cs
+++ 230421/Josephus permutation.cs
@@ -15,27 +15,35 @@ namespace _230421
         public void OneAliveOneKill(string str)
         {
             int totalPeople = int.Parse(str);                   // 참가인원 int형으로 형변환
-            int defaultNum = 2;                                 // 초기 제곱수 변수
+            int defaultNum = 1;                                 // 초기 제곱수 변수
             int needKill = 0;                                   // 2의 제곱수가 남기까지 필요한 제거 수 변수
-            for (int i=1; defaultNum<totalPeople; i++)          // 반복 (2의 i승)
+            if (totalPeople >= 2)
             {
-                if (totalPeople - defaultNum > defaultNum)          // 참가인원 - 현재 제곱수 값이 현재 제곱수 값보다 크면
-                    defaultNum *= 2;                                    // 제곱수 승 증가
-                else if (totalPeople - defaultNum == defaultNum)    // 참가인원 - 현재 제곱수 값이 현재 제곱수 값과 같다면
+                for (int i = 0; defaultNum < totalPeople; i++)          // 반복 (2의 i승)
                 {
-                    lastAliver = 1;                                     // 참가인원이 제곱수 이므로 1번째가 생존자
-                    break;
+                    if (totalPeople - defaultNum > defaultNum)          // 참가인원 - 현재 제곱수 값이 현재 제곱수 값보다 크면
+                        defaultNum *= 2;                                    // 제곱수 승 증가
+                    else if (totalPeople - defaultNum == defaultNum)    // 참가인원 - 현재 제곱수 값이 현재 제곱수 값과 같다면
+                    {
+                        lastAliver = 1;                                     // 참가인원이 제곱수 이므로 1번째가 생존자
+                        break;
+                    }
+                    else                                                // 참가인원 - 현재 제곱수 값이 현재 제곱수 값보다 작으면
+                    {
+                        needKill = totalPeople - defaultNum;                // 필요한 제거 수는 참가인원 - 현재 제곱수
+                        lastAliver = needKill * 2 + 1;                      // 생존자는 제거 수 다음 번째
+                        break;
+                    }
                 }
-                else                                                // 참가인원 - 현재 제곱수 값이 현재 제곱수 값보다 작으면
-                {
-                    needKill = totalPeople - defaultNum;                // 필요한 제거 수는 참가인원 - 현재 제곱수
-                    lastAliver = needKill * 2 + 1;                      // 생존자는 제거 수 다음 번째
-                    break;
-                }
+                // 확인용 출력
+                //Console.WriteLine(defaultNum);
+                Console.WriteLine($"최후의 생존자 : {lastAliver}번");
             }
-            // 확인용 출력
-            //Console.WriteLine(defaultNum);
-            Console.WriteLine($"최후의 생존자 : {lastAliver}번");
+            else
+            {
+                Console.WriteLine($"잠가인원이 부족합니다");
+            }
+            
         }
     }
 }
