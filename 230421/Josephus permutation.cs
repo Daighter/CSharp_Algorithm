2434028b94318e73e89ad6c96f5d213c6b5f0c69
현수 조 <jhsroid@naver.com>
2023-04-23 21:03:10 +0900

    feat : 1번부터 살리고 죽이는 규칙의 함수 추가

--- 230421/Josephus permutation.cs
+++ 230421/Josephus permutation.cs
@@ -8,5 +8,25 @@ namespace _230421
 {
     internal class Josephus_permutation
     {
+        private int lastNumber;
+        public Josephus_permutation() { }
+
+        // 1번부터 살리고 => 죽이고 반복
+        public void LastAliver(string str)
+        {
+            int totalPeople = int.Parse(str);
+            int defaultNum = 2;
+            while(true)
+            {
+                if (totalPeople - defaultNum > defaultNum)
+                    defaultNum *= 2;
+                else
+                    break;
+            }
+            // 확인용 출력
+            Console.WriteLine(defaultNum);
+            lastNumber = defaultNum + 1;
+            Console.WriteLine($"최후의 생존자 : {lastNumber}번");
+        }
     }
 }
