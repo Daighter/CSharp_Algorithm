b175266011d658eba8be9d62335fb22e899c4281
현수 조 <jhsroid@naver.com>
2023-04-23 19:46:24 +0900

    feat : 로딩 대기 플레이어 클래스 추가

--- 230421/FastToStart.cs
+++ 230421/FastToStart.cs
@@ -9,4 +9,20 @@ namespace _230421
     internal class FastToStart
     {
     }
+
+    public class Player
+    {
+        private string name;
+        private int speed;
+        public Player(string name)
+        {
+            AddPlayer();
+        }
+
+        public void AddPlayer()
+        {
+            Random setSpeed = new Random();
+            speed = setSpeed.Next(1, 10);
+        }
+    }
 }
