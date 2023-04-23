19f42bf20d6d542d03e4dcbb3c244a994167eff4
현수 조 <jhsroid@naver.com>
2023-04-23 21:45:43 +0900

    docs : 설명을 주석으로 추가

--- 230421/Josephus permutation.cs
+++ 230421/Josephus permutation.cs
@@ -14,27 +14,27 @@ namespace _230421
         // 1번부터 살리고 => 죽이고 반복
         public void LastAliver(string str)
         {
-            int totalPeople = int.Parse(str);                   // 참가인원 int로 면환
-            int defaultNum = 2;                                 // 2의 제곱수
-            int needKill = 0;
-            for (int i=1; defaultNum<totalPeople; i++)          // 반복(2의 i승)
+            int totalPeople = int.Parse(str);                   // 참가인원 int형으로 형변환
+            int defaultNum = 2;                                 // 초기 제곱수 변수
+            int needKill = 0;                                   // 2의 제곱수가 남기까지 필요한 제거 수 변수
+            for (int i=1; defaultNum<totalPeople; i++)          // 반복 (2의 i승)
             {
-                if (totalPeople - defaultNum > defaultNum)          // 참가인원 - 현재 제곱수 값이 제곱수 보다 크면
-                    defaultNum *= 2;                                    // 제곱수 2배 증가
-                else if (totalPeople - defaultNum == defaultNum)     // 참가인원 - 현재 제곱수 값이 제곱수 보다 작거나 같으면
+                if (totalPeople - defaultNum > defaultNum)          // 참가인원 - 현재 제곱수 값이 현재 제곱수 값보다 크면
+                    defaultNum *= 2;                                    // 제곱수 승 증가
+                else if (totalPeople - defaultNum == defaultNum)    // 참가인원 - 현재 제곱수 값이 현재 제곱수 값과 같다면
                 {
-                    lastNumber = 1;
+                    lastNumber = 1;                                     // 참가인원이 제곱수 이므로 1번째가 생존자
                     break;
                 }
-                else
+                else                                                // 참가인원 - 현재 제곱수 값이 현재 제곱수 값보다 작으면
                 {
-                    needKill = totalPeople - defaultNum;
-                    lastNumber = needKill * 2 + 1;
+                    needKill = totalPeople - defaultNum;                // 필요한 제거 수는 참가인원 - 현재 제곱수
+                    lastNumber = needKill * 2 + 1;                      // 생존자는 제거 수 다음 번째
                     break;
                 }
             }
             // 확인용 출력
-            Console.WriteLine(defaultNum);
+            //Console.WriteLine(defaultNum);
             Console.WriteLine($"최후의 생존자 : {lastNumber}번");
         }
     }
