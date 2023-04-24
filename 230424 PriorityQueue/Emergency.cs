6ebf14dfea644f2d85276957d6c2aa58aad2cc69
현수 조 <jhsroid@naver.com>
2023-04-24 21:34:43 +0900

    feat : 음급실 클래스와 환자 클래스 추가

---
+++ 230424 PriorityQueue/Emergency.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230424_PriorityQueue
+{
+    internal class Emergency
+    {
+        private PriorityQueue<string, int> curePriority = new PriorityQueue<string, int>();
+        private List<int> goldenTimes = new List<int>();
+        public Emergency() { }
+    }
+
+    public class Patient
+    {
+        private string name;
+        private int goldenTime;
+        public Patient(string name)
+        {
+            this.name = name;
+            this.goldenTime = Random();
+        }
+
+        // 랜덤 골든타임 생성
+        internal int Random()
+        {
+            Random random = new Random();
+            return random.Next(1, 100);
+        }
+    }
+}
