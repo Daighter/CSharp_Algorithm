ab86223ad1aec2964ba5a943510a03ebaeb6418c
현수 조 <jhsroid@naver.com>
2023-04-24 23:18:39 +0900

    fix : 메인함수 테스트 수정

--- 230424 PriorityQueue/Program.cs
+++ 230424 PriorityQueue/Program.cs
@@ -9,10 +9,10 @@
             emergency.EnQueuePatient(new Patient("박영희"));
             emergency.EnQueuePatient(new Patient("다니엘잭"));
 
-            emergency.curePriority.Peek();
-
-            while (pq2.Count > 0)
-                Console.WriteLine(pq2.Dequeue()); // 우선순위가 높은 순서대로 데이터 출력
+            while (emergency.curePriority.Count > 0)
+            {
+                Console.WriteLine(emergency.curePriority.Dequeue()); // 우선순위가 높은 순서대로 데이터 출력
+            }
         }
     }
 }
