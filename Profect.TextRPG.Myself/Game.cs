605573c7fd3b2d5a60d05d182529d26c581654ad
현수 조 <jhsroid@naver.com>
2023-05-05 14:46:14 +0900

    feat : 게임 클래스 추가

--- Profect.TextRPG.Myself/Game.cs
+++ Profect.TextRPG.Myself/Game.cs
@@ -8,5 +8,44 @@ namespace Profect.TextRPG.Myself
 {
     internal class Game
     {
+        private bool running = true;
+
+        public void Run()
+        {
+            Init();                 // 초기화
+
+            while (running)         // 실행 후 반복
+            {
+                Render();               // 출력
+
+                Update();               // 갱신
+            }
+
+            Release();              // 마무리
+        }
+
+        // 게임 초기화
+        private void Init()
+        {
+
+        }
+
+        // 게임 출력
+        private void Render()
+        {
+
+        }
+
+        // 게임 갱신
+        private void Update()
+        {
+
+        }
+
+        // 게임 마무리
+        private void Release()
+        {
+
+        }
     }
 }
