e5ebd2da0ee1821d94bce0514c488f5643944a82
현수 조 <jhsroid@naver.com>
2023-05-05 16:19:02 +0900

    feat : 맵씬과 스테이지 클래스 추가

---
+++ Profect.TextRPG.Myself/Scene/MapScene.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profect.TextRPG.Myself
+{
+    internal class MapScene : Scene
+    {
+        public MapScene(Game game) : base(game) { } 
+
+        // Scene 오버라이드
+        public override void Render()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Scene 오버라이드
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
