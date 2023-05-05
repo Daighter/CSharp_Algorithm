e76faad35e1d0083021ae64ce81756be0aea86a2
현수 조 <jhsroid@naver.com>
2023-05-05 14:47:00 +0900

    feat : Scene, MainMenuScene 클래스 추가

---
+++ Profect.TextRPG.Myself/Scene/MainMenuScene.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profect.TextRPG.Myself
+{
+    internal class MainMenuScene : Scene
+    {
+        public override void Render()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Profect.TextRPG.Myself/Scene/Scene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profect.TextRPG.Myself
+{
+    internal abstract class Scene
+    {
+        public Scene() { }
+
+        public abstract void Render();
+
+        public abstract void Update();
+    }
+}
