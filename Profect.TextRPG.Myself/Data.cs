4c50d85e9247f95b882d2706770dd8aae272a1a9
현수 조 <jhsroid@naver.com>
2023-05-08 16:08:01 +0900

    feat : 몬스터 추상클래스 추가

--- Profect.TextRPG.Myself/Data.cs
+++ Profect.TextRPG.Myself/Data.cs
@@ -11,10 +11,11 @@ namespace Profect.TextRPG.Myself
     {
         public static bool[,] map;
         public static Player player;
+        public static List<Monster> monsters;
 
         public static void Init()
         {
-
+            monsters = new List<Monster>();
         }
 
         public static void SetPlayer(ConsoleKeyInfo input, string name)
@@ -37,6 +38,24 @@ namespace Profect.TextRPG.Myself
             map = Stage1.map;
 
             player.Pos = new Position(2, 2);
+        }
+
+        // 매개변수 좌표에 몬스터가 없으면 true
+        public static bool IsObjectInPos(Position pos)
+        {
+            return MonsterInPos(pos) == null;
+        }
+
+        // 매개변수 자리에 몬스터가 있으면 몬스터 반환
+        public static Monster MonsterInPos(Position pos)
+        {
+            foreach (Monster monster in monsters)           // 모든 리스트<몬스터> 검사
+            {
+                if (monster.Pos.x == pos.x &&                   // 몬스터의 좌표와 같으면
+                    monster.Pos.y == pos.y)
+                    return monster;                                 // 몬스터 반환
+            }
+            return null;                                    // 없으면 null 반환
         }
     }
 }
---
+++ Profect.TextRPG.Myself/Monster/Monster.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profect.TextRPG.Myself
+{
+    internal abstract class Monster
+    {
+        protected string name;
+        protected string image;
+        protected char icon = '▼';
+        protected Position pos;
+        protected int maxHp;
+        protected int curHp;
+        protected int ap;
+        protected int dp;
+
+        public Monster() { }
+
+        public string Name { get { return name; } }
+        public char Icon { get { return icon; } }
+        public Position Pos { get { return pos; } set { pos = value; } }
+        public int CurHp { get { return curHp; } }
+        public int MaxHp { get { return maxHp; } }
+        public int AP { get { return ap; } }
+        public int DP { get { return dp; } }
+
+        // 이동
+        public void Move(Direction dir)
+        {
+            Position prevPos = pos;
+
+            switch (dir)
+            {
+                case Direction.Up:
+                    pos.y--;
+                    break;
+                case Direction.Down:
+                    pos.y++;
+                    break;
+                case Direction.Left:
+                    pos.x--;
+                    break;
+                case Direction.Right:
+                    pos.x++;
+                    break;
+            }
+
+            if (!Data.map[pos.y, pos.x] ||          // 이동한 자리가 벽이거나
+                Data.IsObjectInPos(pos))            // 다른 물체가 있을 때
+                pos = prevPos;                          // 저장위치로 복귀
+        }
+    }
+}
